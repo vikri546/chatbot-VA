fbc1e41923776296d6c8260789b68a160a9759b0
vikri546 <vikriardiansyah3162@gmail.com>
2026-04-11 06:36:49 +0700

    Fix: Chat Gemini API

--- config.example.js
+++ config.example.js
@@ -4,10 +4,10 @@
  *   cp config.example.js config.js
  */
 module.exports = {
-    WEATHER_API_KEY: process.env.WEATHER_API_KEY || '',
-    ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || '',
-    ELEVENLABS_VOICE_ID: process.env.ELEVENLABS_VOICE_ID || '',
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY || '',
-    STICKER_PACK_NAME: 'Copyright VA 2026',
-    STICKER_AUTHOR: '',
+    WEATHER_API_KEY: process.env.WEATHER_API_KEY || '',             // OpenWeatherMap API Key
+    ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || '',       // ElevenLabs API Key
+    ELEVENLABS_VOICE_ID: process.env.ELEVENLABS_VOICE_ID || '',     // ElevenLabs Voice ID
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY || '',               // Gemini API Key
+    STICKER_PACK_NAME: 'Copyright VA 2026',                         // Nama pack stiker
+    STICKER_AUTHOR: '',                                             // Author stiker
 };
